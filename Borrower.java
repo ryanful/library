82681c90d9193e9297353335d67320291516c1d6
ryanful <ryanful64@gmail.com>
2016-03-05 20:03:27 -0500

    Update Borrower.java

--- Borrower.java
+++ Borrower.java
@@ -2,7 +2,11 @@ public abstract class Borrower {
   
   public void browse(){
  
-   }
+  }
+  
+  public abstract void browse(String x){};
+  public abstract void checkOut(){};
+  public abstract void returnBook(){};
 
   
   
